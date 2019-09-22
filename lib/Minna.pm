1bc30a2cd8e139743c6b8bf50b8ce26bcdaeb53c
d0p1 <contact@d0p1.eu>
2019-09-22 15:31:23 +0900

    add count of registered and banned instances on index page

--- lib/Minna.pm
+++ lib/Minna.pm
@@ -1,10 +1,24 @@
 package Minna;
 use Dancer2;
+use FindBin;
+use DBI;
 
 our $VERSION = '0.1';
 
 get '/' => sub {
-    template 'index' => { 'title' => 'みんな - ActivityPub Relay' };
+    my $db = DBI->connect("dbi:SQLite:dbname=$FindBin::Bin/../db.sqlite") or die DBI::errstr;
+    my $query = $db->prepare('SELECT COUNT(*) FROM following') or die $db->errstr;
+    $query->execute() or die $query->errstr;
+    my ($count_registered) = $query->fetchrow_array;
+
+    $query = $db->prepare('SELECT COUNT(*) FROM ban') or die $db->errstr;
+    $query->execute() or die $query->errstr;
+    my ($count_banned) = $query->fetchrow_array;
+    template 'index' => {
+        'title' => 'みんな - ActivityPub Relay',
+        'count_registered' => $count_registered,
+        'count_banned' => $count_banned
+    };
 };
 
 true;
